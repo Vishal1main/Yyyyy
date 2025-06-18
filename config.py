f6b561d63d9b4b9d2e33dad9989945ae803de1c8
Vishal1main <monsterr258258@gmail.com>
2025-06-18 20:11:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,2 @@
 BOT_TOKEN = "7861502352:AAFcS7xZk2NvN7eJ3jcPm_HyYh74my8vRyU"
-WEBHOOK_URL = "https://yyyyy-cpnv.onrender.com/webhook"
-PORT = 8443  # or any free port
+WEBHOOK_PATH = f"/webhook/{BOT_TOKEN}"
