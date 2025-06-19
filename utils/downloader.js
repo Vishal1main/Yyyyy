f564b4dc19d75aee49a2fde21495525aecb51728
Vishal1main <monsterr258258@gmail.com>
2025-06-19 13:59:37 +0530

    Update downloader.js

--- utils/downloader.js
+++ utils/downloader.js
@@ -3,15 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 async function downloadFile(url, tempDir) {
-  const parsedUrl = new URL(url);
-  let filename = path.basename(parsedUrl.pathname);
-  
-  if (!filename || filename === parsedUrl.hostname || !path.extname(filename)) {
-    const contentType = await getContentType(url);
-    const ext = contentType.split('/').pop();
-    filename = `file_${Date.now()}.${ext}`;
-  }
-
+  const filename = path.basename(new URL(url).pathname) || `file_${Date.now()}`;
   const filePath = path.join(tempDir, filename);
   const writer = fs.createWriteStream(filePath);
   
@@ -29,15 +21,6 @@ async function downloadFile(url, tempDir) {
   });
 
   return { filePath, originalName: filename };
-}
-
-async function getContentType(url) {
-  try {
-    const response = await axios.head(url);
-    return response.headers['content-type'] || 'application/octet-stream';
-  } catch (error) {
-    return 'application/octet-stream';
-  }
 }
 
 module.exports = { downloadFile };
