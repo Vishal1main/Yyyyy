773f9cda4badc881ab46d87162d03528b5254a12
Vishal1main <monsterr258258@gmail.com>
2025-06-19 14:25:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ module.exports = {
   BOT_TOKEN: '7861502352:AAF09jfPpjU78dnwl4NiM95TadZAE6kjo1M',
   MAX_FILE_SIZE: 2000, // MB
   TEMP_DIR: './temp',
-  ALLOWED_USERS: [],
+  DOWNLOAD_TIMEOUT: 300000, // 5 minutes
+  UPLOAD_TIMEOUT: 600000,    // 10 minutes,
   PORT: process.env.PORT || 3000,
   WEBHOOK_URL: 'https://yyyyy-er7k.onrender.com' // Optional for webhooks
 };
